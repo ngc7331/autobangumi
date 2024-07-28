3f8d0c16da54f3cf890f4bfd90194176462e03d2
ngc7331 <ngc7331@outlook.com>
2024-07-28 12:54:38 +0800

    refactor: split bangumi.py

--- autobangumi.py
+++ autobangumi.py
@@ -5,6 +5,7 @@ from qbittorrentapi import TorrentDictionary
 import re
 from typing import List, Dict, Literal
 
+from module.bangumi import Bangumi
 from module.qbittorrent import QBConfig, QBWorker
 
 
@@ -21,70 +22,6 @@ class Config(QBConfig):
     })
     library: str = "/jellyfin/anime"
 
-
-@dataclass
-class Bangumi:
-    type: Literal["single", "multi"] | None = None
-    groups: List[str] | None = None
-    name: str | None = None
-    season: int = -1
-    episode: int = -1  # valid for type == "single"
-    version: int = -1  # valid for type == "single"
-    meta: List[str] | None = None
-    ext: str | None = None
-
-    @staticmethod
-    def parse_multi(name: str) -> "Bangumi":
-        for pattern in [
-            # [group] name [episodes][meta]
-            re.compile(r"\[(.*?)\] (.*?)(?: (?:S(?:eason ?)?)?(\d+))? ?(?:\[(?:\d+)-(?:\d+)(?: ?[Ff][Ii][Nn])(?:\+SP)??\])?\[(.*)\]"),
-        ]:
-            match = pattern.match(name)
-            if match:
-                return Bangumi(
-                    type="multi",
-                    groups=match.group(1).split("&"),
-                    name=match.group(2),
-                    season=1 if match.group(3) is None else int(match.group(3)),
-                    meta=match.group(4).replace("][", " ").split(),
-                )
-        raise ValueError(f"Failed to match name: {name}")
-
-    @staticmethod
-    def parse_single(name: str) -> "Bangumi":
-        for pattern in [
-            # [group] name - episode [meta].ext
-            re.compile(r"\[(.*?)\] (.*?)(?: (?:S(?:eason ?)?)?(\d+))? - (\d+)(?:v(\d+))? \[(.*)\]\.(.*)"),
-            # [group] name [episode][meta].ext
-            re.compile(r"\[(.*?)\] (.*?)(?: (?:S(?:eason ?)?)?(\d+))? \[(\d+)(?:v(\d+))?\]\[(.*)\]\.(.*)"),
-        ]:
-            match = pattern.match(name)
-            if match:
-                return Bangumi(
-                    type="single",
-                    groups=match.group(1).split("&"),
-                    name=match.group(2),
-                    season=1 if match.group(3) is None else int(match.group(3)),
-                    episode=int(match.group(4)),
-                    version=1 if match.group(5) is None else int(match.group(5)),
-                    meta=match.group(6).replace("][", " ").split(),
-                    ext=match.group(7),
-                )
-        raise ValueError(f"Failed to match name: {name}")
-
-    @staticmethod
-    def parse(name: str, preffered: Literal["single", "multi"] = "single") -> "Bangumi":
-        if preffered == "single":
-            first = Bangumi.parse_single
-            second = Bangumi.parse_multi
-        elif preffered == "multi":
-            first = Bangumi.parse_multi
-            second = Bangumi.parse_single
-
-        try:
-            return first(name)
-        except ValueError:
-            return second(name)
 
 class Worker(QBWorker):
     def __init__(self, config: Config):
